1faee3e7971fc619a8a401105f3c5132bab00ad9
wassim.mansouri <wassim.mansouri@imaginepartners.com>
2017-03-12 18:36:27 +0100

    Add the thumbnail component

--- app/events/events-list.component.ts
+++ app/events/events-list.component.ts
@@ -7,22 +7,12 @@ import { Component } from '@angular/core'
         <div>
             <h1>Upcoming Angular 2 Events</h1>
             <hr/>
-            <div class="well hoverwell thumbnail">
-                <h2>{{event.name}}</h2>
-                <div>Date: {{event.date}}</div>
-                <div>Time: {{event.time}}</div>
-                <div>Price: \${{event.price}}</div>
-                <div>
-                    <span>Location: {{event.location.addresse}}</span>
-                    <span>&nbsp;</span>
-                    <span>{{event.location.city}}, {{event.location.country}}</span>
-                </div>
-            </div>
+            <event-thumbnail [event]="event1"></event-thumbnail>
         </div>
     `
 })
 export class EventsListComponent {
-    event = {
+    event1 = {
         id: 1,
         name: 'Angular Connect',
         date: '9/26/2036',
