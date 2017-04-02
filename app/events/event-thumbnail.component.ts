00d8aa6824a82f1957868afe61876381ecc3688e
wassim.mansouri <wassim.mansouri@imaginepartners.com>
2017-04-02 08:54:24 +0100

    Add Events Service

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,18 +4,26 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
     selector: 'event-thumbnail',
     template: `
         <div class="well hoverwell thumbnail">
-            <h2>{{event.name}}</h2>
-            <div>Date: {{event.date}}</div>
-            <div>Time: {{event.time}}</div>
-            <div>Price: \${{event.price}}</div>
-            <div>
-                <span>Location: {{event.location.address}}</span>
-                
-                <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <h2>{{event?.name}}</h2>
+            <div>Date: {{event?.date}}</div>
+            <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+                Time: {{event?.time}}
+                <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
+                <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
+                <span *ngSwitchDefault>(Normal Start)</span>
             </div>
+            <div>Price: \${{event?.price}}</div>
+            <div *ngIf="event?.location">
+                <span>Location: {{event?.location?.address}}</span>
+                
+                <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
+            </div>
+            <div *ngIf="event?.onlineUrl">Online URL: {{event?.onlineUrl}}</div>
         </div>
     `,
     styles: [`
+        .green {color: #003300 !important;}
+        .bold {font-weight: bold;}
         .pad-left {margin-left: 10px;}
         .well div {color: #bbb}
         .thumbnail {min-height: 210px;}
@@ -23,4 +31,9 @@ import {Component, Input, Output, EventEmitter} from '@angular/core'
 })
 export class EventThumbnailComponent {
     @Input() event:any
+
+    getStartTimeClass() {
+        const isEarlyStart = this.event && this.event.time === '8:00 am'
+        return {green: isEarlyStart, bold: isEarlyStart}
+    }
 }
