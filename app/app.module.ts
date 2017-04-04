4ecd06f595a0d57e95eca1623fba1569720abe02
wassim.mansouri <wassim.mansouri@imaginepartners.com>
2017-04-04 09:28:49 +0100

    Add export barrels

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
+
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventService,
+    EventRouteActivator,
+    EventsListResolver
+} from './events/index'
+
 import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
-import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
-import { EventRouteActivator } from './events/event-route-activator.service'
-import { EventsListResolver } from './events/events-list-resolver.service'
 import { appRoutes } from './routes'
-import { ProfileComponent } from "./user/profile.component";
+import { ProfileComponent } from "./user/profile.component"
 
 @NgModule({
     imports: [
--- app/events/event-details/event-route-activator.service.ts
+++ app/events/event-details/event-route-activator.service.ts
@@ -1,6 +1,6 @@
 import { Router, ActivatedRouteSnapshot, CanActivate } from '@angular/router'
 import { Injectable } from '@angular/core'
-import { EventService } from './shared/event.service'
+import { EventService } from '../shared/event.service'
 
 @Injectable()
 export class EventRouteActivator implements CanActivate {
